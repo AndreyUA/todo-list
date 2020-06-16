e6acf7fc130c0bb926e88e11b1c3248d2109cffa
AndreyUA <infernalsteel666@gmail.com>
2020-06-16 22:13:29 +0300

    Updates

---
+++ precache-manifest.26444a5f0020b5bd5346448aea552d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b618e1e3d81d9dd402a80acf335e4fe",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "aa0571279187030eadc2",
+    "url": "/todo-list/static/css/main.10c9be69.chunk.css"
+  },
+  {
+    "revision": "b222412728111ad65eaa",
+    "url": "/todo-list/static/js/2.4892749c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.4892749c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0571279187030eadc2",
+    "url": "/todo-list/static/js/main.4088a7ef.chunk.js"
+  },
+  {
+    "revision": "da2a803dcedd98b44d11",
+    "url": "/todo-list/static/js/runtime-main.a19f0868.js"
+  }
+]);
